7e56193972909de8b6ab36ff4fd8d9f4c6202dbd
Michael Cooper <CPM@braybrooksc.vic.edu.au>
2019-04-03 14:03:17 +1100

    Add project files.

---
+++ KK7 - Searching Practice/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace KK7___Searching_Practice
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LinearFoundHeadingLabel = new System.Windows.Forms.Label();
+            this.LinearFoundResultLabel = new System.Windows.Forms.Label();
+            this.HeadingLabel = new System.Windows.Forms.Label();
+            this.BinaryFoundResultLabel = new System.Windows.Forms.Label();
+            this.BinaryFoundHeadingLabel = new System.Windows.Forms.Label();
+            this.LinearPassResultLabel = new System.Windows.Forms.Label();
+            this.LinearPassHeadlingLabel = new System.Windows.Forms.Label();
+            this.BinaryPassResultLabel = new System.Windows.Forms.Label();
+            this.BinaryPassHeadingLabel = new System.Windows.Forms.Label();
+            this.SearchNumberTextBox = new System.Windows.Forms.TextBox();
+            this.BinarySearchHeadingLabel = new System.Windows.Forms.Label();
+            this.ArraySizeHeadingLabel = new System.Windows.Forms.Label();
+            this.ArraySizeTextBox = new System.Windows.Forms.TextBox();
+            this.MaxValuesHeadingLabel = new System.Windows.Forms.Label();
+            this.MaxIntTextBox = new System.Windows.Forms.TextBox();
+            this.LinearHeadingLabel = new System.Windows.Forms.Label();
+            this.BinaryHeadingLabel = new System.Windows.Forms.Label();
+            this.CreateSearchCountButton = new System.Windows.Forms.Button();
+            this.LinearCountResultLabel = new System.Windows.Forms.Label();
+            this.LinearCountHeadingLabel = new System.Windows.Forms.Label();
+            this.CreateSearchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LinearFoundHeadingLabel
+            // 
+            this.LinearFoundHeadingLabel.AutoSize = true;
+            this.LinearFoundHeadingLabel.Location = new System.Drawing.Point(113, 599);
+            this.LinearFoundHeadingLabel.Name = "LinearFoundHeadingLabel";
+            this.LinearFoundHeadingLabel.Size = new System.Drawing.Size(119, 20);
+            this.LinearFoundHeadingLabel.TabIndex = 1;
+            this.LinearFoundHeadingLabel.Text = "Number found?";
+            // 
+            // LinearFoundResultLabel
+            // 
+            this.LinearFoundResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LinearFoundResultLabel.Location = new System.Drawing.Point(125, 634);
+            this.LinearFoundResultLabel.Name = "LinearFoundResultLabel";
+            this.LinearFoundResultLabel.Size = new System.Drawing.Size(100, 50);
+            this.LinearFoundResultLabel.TabIndex = 2;
+            this.LinearFoundResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // HeadingLabel
+            // 
+            this.HeadingLabel.AutoSize = true;
+            this.HeadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeadingLabel.Location = new System.Drawing.Point(231, 28);
+            this.HeadingLabel.Name = "HeadingLabel";
+            this.HeadingLabel.Size = new System.Drawing.Size(250, 55);
+            this.HeadingLabel.TabIndex = 3;
+            this.HeadingLabel.Text = "Searching";
+            // 
+            // BinaryFoundResultLabel
+            // 
+            this.BinaryFoundResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BinaryFoundResultLabel.Location = new System.Drawing.Point(450, 634);
+            this.BinaryFoundResultLabel.Name = "BinaryFoundResultLabel";
+            this.BinaryFoundResultLabel.Size = new System.Drawing.Size(100, 50);
+            this.BinaryFoundResultLabel.TabIndex = 8;
+            this.BinaryFoundResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BinaryFoundHeadingLabel
+            // 
+            this.BinaryFoundHeadingLabel.AutoSize = true;
+            this.BinaryFoundHeadingLabel.Location = new System.Drawing.Point(438, 599);
+            this.BinaryFoundHeadingLabel.Name = "BinaryFoundHeadingLabel";
+            this.BinaryFoundHeadingLabel.Size = new System.Drawing.Size(119, 20);
+            this.BinaryFoundHeadingLabel.TabIndex = 11;
+            this.BinaryFoundHeadingLabel.Text = "Number found?";
+            // 
+            // LinearPassResultLabel
+            // 
+            this.LinearPassResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LinearPassResultLabel.Location = new System.Drawing.Point(125, 737);
+            this.LinearPassResultLabel.Name = "LinearPassResultLabel";
+            this.LinearPassResultLabel.Size = new System.Drawing.Size(100, 50);
+            this.LinearPassResultLabel.TabIndex = 13;
+            this.LinearPassResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LinearPassHeadlingLabel
+            // 
+            this.LinearPassHeadlingLabel.AutoSize = true;
+            this.LinearPassHeadlingLabel.Location = new System.Drawing.Point(143, 703);
+            this.LinearPassHeadlingLabel.Name = "LinearPassHeadlingLabel";
+            this.LinearPassHeadlingLabel.Size = new System.Drawing.Size(65, 20);
+            this.LinearPassHeadlingLabel.TabIndex = 12;
+            this.LinearPassHeadlingLabel.Text = "Passes:";
+            // 
+            // BinaryPassResultLabel
+            // 
+            this.BinaryPassResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BinaryPassResultLabel.Location = new System.Drawing.Point(450, 737);
+            this.BinaryPassResultLabel.Name = "BinaryPassResultLabel";
+            this.BinaryPassResultLabel.Size = new System.Drawing.Size(100, 50);
+            this.BinaryPassResultLabel.TabIndex = 15;
+            this.BinaryPassResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BinaryPassHeadingLabel
+            // 
+            this.BinaryPassHeadingLabel.AutoSize = true;
+            this.BinaryPassHeadingLabel.Location = new System.Drawing.Point(468, 702);
+            this.BinaryPassHeadingLabel.Name = "BinaryPassHeadingLabel";
+            this.BinaryPassHeadingLabel.Size = new System.Drawing.Size(65, 20);
+            this.BinaryPassHeadingLabel.TabIndex = 14;
+            this.BinaryPassHeadingLabel.Text = "Passes:";
+            // 
+            // SearchNumberTextBox
+            // 
+            this.SearchNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SearchNumberTextBox.Location = new System.Drawing.Point(450, 229);
+            this.SearchNumberTextBox.Name = "SearchNumberTextBox";
+            this.SearchNumberTextBox.Size = new System.Drawing.Size(100, 35);
+            this.SearchNumberTextBox.TabIndex = 3;
+            this.SearchNumberTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // BinarySearchHeadingLabel
+            // 
+            this.BinarySearchHeadingLabel.AutoSize = true;
+            this.BinarySearchHeadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinarySearchHeadingLabel.Location = new System.Drawing.Point(169, 229);
+            this.BinarySearchHeadingLabel.Name = "BinarySearchHeadingLabel";
+            this.BinarySearchHeadingLabel.Size = new System.Drawing.Size(265, 29);
+            this.BinarySearchHeadingLabel.TabIndex = 10;
+            this.BinarySearchHeadingLabel.Text = "Number to search for:";
+            // 
+            // ArraySizeHeadingLabel
+            // 
+            this.ArraySizeHeadingLabel.AutoSize = true;
+            this.ArraySizeHeadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ArraySizeHeadingLabel.Location = new System.Drawing.Point(35, 113);
+            this.ArraySizeHeadingLabel.Name = "ArraySizeHeadingLabel";
+            this.ArraySizeHeadingLabel.Size = new System.Drawing.Size(395, 29);
+            this.ArraySizeHeadingLabel.TabIndex = 18;
+            this.ArraySizeHeadingLabel.Text = "Size of array (up to 100,000,000):";
+            // 
+            // ArraySizeTextBox
+            // 
+            this.ArraySizeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ArraySizeTextBox.Location = new System.Drawing.Point(448, 113);
+            this.ArraySizeTextBox.Name = "ArraySizeTextBox";
+            this.ArraySizeTextBox.Size = new System.Drawing.Size(181, 35);
+            this.ArraySizeTextBox.TabIndex = 1;
+            this.ArraySizeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MaxValuesHeadingLabel
+            // 
+            this.MaxValuesHeadingLabel.AutoSize = true;
+            this.MaxValuesHeadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxValuesHeadingLabel.Location = new System.Drawing.Point(118, 169);
+            this.MaxValuesHeadingLabel.Name = "MaxValuesHeadingLabel";
+            this.MaxValuesHeadingLabel.Size = new System.Drawing.Size(314, 29);
+            this.MaxValuesHeadingLabel.TabIndex = 20;
+            this.MaxValuesHeadingLabel.Text = "Maximum random integer:";
+            // 
+            // MaxIntTextBox
+            // 
+            this.MaxIntTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MaxIntTextBox.Location = new System.Drawing.Point(450, 169);
+            this.MaxIntTextBox.Name = "MaxIntTextBox";
+            this.MaxIntTextBox.Size = new System.Drawing.Size(181, 35);
+            this.MaxIntTextBox.TabIndex = 2;
+            this.MaxIntTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // LinearHeadingLabel
+            // 
+            this.LinearHeadingLabel.AutoSize = true;
+            this.LinearHeadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LinearHeadingLabel.Location = new System.Drawing.Point(88, 559);
+            this.LinearHeadingLabel.Name = "LinearHeadingLabel";
+            this.LinearHeadingLabel.Size = new System.Drawing.Size(175, 29);
+            this.LinearHeadingLabel.TabIndex = 21;
+            this.LinearHeadingLabel.Text = "Linear Search";
+            // 
+            // BinaryHeadingLabel
+            // 
+            this.BinaryHeadingLabel.AutoSize = true;
+            this.BinaryHeadingLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BinaryHeadingLabel.Location = new System.Drawing.Point(413, 559);
+            this.BinaryHeadingLabel.Name = "BinaryHeadingLabel";
+            this.BinaryHeadingLabel.Size = new System.Drawing.Size(175, 29);
+            this.BinaryHeadingLabel.TabIndex = 22;
+            this.BinaryHeadingLabel.Text = "Binary Search";
+            // 
+            // CreateSearchCountButton
+            // 
+            this.CreateSearchCountButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateSearchCountButton.Location = new System.Drawing.Point(40, 417);
+            this.CreateSearchCountButton.Name = "CreateSearchCountButton";
+            this.CreateSearchCountButton.Size = new System.Drawing.Size(589, 106);
+            this.CreateSearchCountButton.TabIndex = 23;
+            this.CreateSearchCountButton.Text = "Create integer array, linear search, and count";
+            this.CreateSearchCountButton.UseVisualStyleBackColor = true;
+            this.CreateSearchCountButton.Click += new System.EventHandler(this.CreateSearchCountButton_Click);
+            // 
+            // LinearCountResultLabel
+            // 
+            this.LinearCountResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LinearCountResultLabel.Location = new System.Drawing.Point(125, 852);
+            this.LinearCountResultLabel.Name = "LinearCountResultLabel";
+            this.LinearCountResultLabel.Size = new System.Drawing.Size(100, 50);
+            this.LinearCountResultLabel.TabIndex = 25;
+            this.LinearCountResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // LinearCountHeadingLabel
+            // 
+            this.LinearCountHeadingLabel.AutoSize = true;
+            this.LinearCountHeadingLabel.Location = new System.Drawing.Point(147, 817);
+            this.LinearCountHeadingLabel.Name = "LinearCountHeadingLabel";
+            this.LinearCountHeadingLabel.Size = new System.Drawing.Size(56, 20);
+            this.LinearCountHeadingLabel.TabIndex = 24;
+            this.LinearCountHeadingLabel.Text = "Count:";
+            // 
+            // CreateSearchButton
+            // 
+            this.CreateSearchButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CreateSearchButton.Location = new System.Drawing.Point(40, 288);
+            this.CreateSearchButton.Name = "CreateSearchButton";
+            this.CreateSearchButton.Size = new System.Drawing.Size(589, 106);
+            this.CreateSearchButton.TabIndex = 26;
+            this.CreateSearchButton.Text = "Create integer array and search (linear and binary)";
+            this.CreateSearchButton.UseVisualStyleBackColor = true;
+            this.CreateSearchButton.Click += new System.EventHandler(this.CreateSearchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(666, 969);
+            this.Controls.Add(this.CreateSearchButton);
+            this.Controls.Add(this.LinearCountResultLabel);
+            this.Controls.Add(this.LinearCountHeadingLabel);
+            this.Controls.Add(this.CreateSearchCountButton);
+            this.Controls.Add(this.BinaryHeadingLabel);
+            this.Controls.Add(this.LinearHeadingLabel);
+            this.Controls.Add(this.MaxValuesHeadingLabel);
+            this.Controls.Add(this.MaxIntTextBox);
+            this.Controls.Add(this.ArraySizeHeadingLabel);
+            this.Controls.Add(this.ArraySizeTextBox);
+            this.Controls.Add(this.BinaryPassResultLabel);
+            this.Controls.Add(this.BinaryPassHeadingLabel);
+            this.Controls.Add(this.LinearPassResultLabel);
+            this.Controls.Add(this.LinearPassHeadlingLabel);
+            this.Controls.Add(this.BinaryFoundHeadingLabel);
+            this.Controls.Add(this.BinarySearchHeadingLabel);
+            this.Controls.Add(this.SearchNumberTextBox);
+            this.Controls.Add(this.BinaryFoundResultLabel);
+            this.Controls.Add(this.HeadingLabel);
+            this.Controls.Add(this.LinearFoundResultLabel);
+            this.Controls.Add(this.LinearFoundHeadingLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label LinearFoundHeadingLabel;
+        private System.Windows.Forms.Label LinearFoundResultLabel;
+        private System.Windows.Forms.Label HeadingLabel;
+        private System.Windows.Forms.Label BinaryFoundResultLabel;
+        private System.Windows.Forms.Label BinaryFoundHeadingLabel;
+        private System.Windows.Forms.Label LinearPassResultLabel;
+        private System.Windows.Forms.Label LinearPassHeadlingLabel;
+        private System.Windows.Forms.Label BinaryPassResultLabel;
+        private System.Windows.Forms.Label BinaryPassHeadingLabel;
+        private System.Windows.Forms.TextBox SearchNumberTextBox;
+        private System.Windows.Forms.Label BinarySearchHeadingLabel;
+        private System.Windows.Forms.Label ArraySizeHeadingLabel;
+        private System.Windows.Forms.TextBox ArraySizeTextBox;
+        private System.Windows.Forms.Label MaxValuesHeadingLabel;
+        private System.Windows.Forms.TextBox MaxIntTextBox;
+        private System.Windows.Forms.Label LinearHeadingLabel;
+        private System.Windows.Forms.Label BinaryHeadingLabel;
+        private System.Windows.Forms.Button CreateSearchCountButton;
+        private System.Windows.Forms.Label LinearCountResultLabel;
+        private System.Windows.Forms.Label LinearCountHeadingLabel;
+        private System.Windows.Forms.Button CreateSearchButton;
+    }
+}
+
---
+++ KK7 - Searching Practice/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KK7___Searching_Practice
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreateSearchButton_Click(object sender, EventArgs e)
+        {
+            LinearCountResultLabel.Text = "";
+            int[] array = CreateIntArray();
+            LinearSearch(array);
+            BinarySearch(array);
+        }
+
+        private void CreateSearchCountButton_Click(object sender, EventArgs e)
+        {
+            BinaryFoundResultLabel.Text = "";
+            BinaryPassResultLabel.Text = "";
+            int[] array = CreateIntArray();
+            LinearSearchCount(array);
+        }
+
+        int[] CreateIntArray()
+        {
+            Random randInt = new Random();
+
+            int arraySize = int.Parse(ArraySizeTextBox.Text);
+            int maxInt = int.Parse(MaxIntTextBox.Text);
+    
+            int[] intArray = new int[arraySize];
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = randInt.Next(0, maxInt + 1);
+            }
+
+            Array.Sort(intArray);
+
+            return intArray;
+        }
+        
+        void LinearSearch(int[] allgrades)
+        {
+            int pass = 0;
+            bool found = false;
+            int searchNumber = int.Parse(SearchNumberTextBox.Text);
+
+            while(found == false && pass < allgrades.Length)
+            {
+                if (allgrades[pass] == searchNumber)
+                {
+                    found = true;
+                }
+                pass++;
+            }
+
+            if (found == true)
+            {
+                LinearFoundResultLabel.Text = "Yes";
+                LinearPassResultLabel.Text = pass.ToString();
+            }
+            else
+            {
+                LinearFoundResultLabel.Text = "No";
+                LinearPassResultLabel.Text = pass.ToString();
+            }
+        }
+
+        void LinearSearchCount(int[] allgrades)
+        {
+            int pass = 0;
+            int count = 0;
+            int searchNumber = int.Parse(SearchNumberTextBox.Text);
+
+            while (pass < allgrades.Length)
+            {
+                if (allgrades[pass] == searchNumber)
+                {
+                    count++;
+                }
+                pass++;
+            }
+
+            if (count > 0)
+            {
+                LinearFoundResultLabel.Text = "Yes";
+                LinearPassResultLabel.Text = pass.ToString();
+                LinearCountResultLabel.Text = count.ToString();
+            }
+            else
+            {
+                LinearFoundResultLabel.Text = "No";
+                LinearPassResultLabel.Text = pass.ToString();
+            }
+        }
+
+        void BinarySearch(int[] intArray)
+        {
+            int low = 0;
+            int high = intArray.Length - 1;
+            int mid = 0;
+            int pass = 0;
+            bool found = false;
+            int searchNumber = int.Parse(SearchNumberTextBox.Text);
+
+            while (low <= high && found == false)
+            {
+                pass++;
+                mid = (low + high) / 2;
+                Console.WriteLine("low = " + low + " mid = " + mid + " high = " + high + " pass = " + pass);
+                
+                if (searchNumber == intArray[mid])
+                {
+                    found = true;
+                }
+
+                else if(searchNumber < intArray[mid])
+                {
+                    high = mid - 1;
+                }
+
+                else if(searchNumber > intArray[mid])
+                {
+                    low = mid + 1;
+                }
+            }
+
+            if (found == true)
+            {
+                BinaryFoundResultLabel.Text = "Yes";
+                BinaryPassResultLabel.Text = pass.ToString();
+                Console.WriteLine("Found");
+            }
+            else
+            {
+                BinaryFoundResultLabel.Text = "No";
+                BinaryPassResultLabel.Text = pass.ToString();
+                Console.WriteLine("Not Found");
+            }
+        }
+    }
+}
